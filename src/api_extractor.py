76189444d70f1a44000d01752a6fa2f9d1ba7b46
Pai <ppeng24@wisc.edu>
2024-07-25 21:46:09 +0800

    (no commit message provided)

--- src/api_extractor.py
+++ src/api_extractor.py
@@ -21,3 +21,23 @@ def fetch_financial_metrics(symbol):
 fetch_historical_prices('TSLA')
 fetch_financial_metrics('TSLA')
  
+ 
+ 
+ 
+from gradio_client import Client
+import json
+
+client = Client("https://fingpt-fingpt-forecaster.hf.space/--replicas/me7ol/")
+result = client.predict(
+        "TSLA", # str  in 'Ticker' Textbox component
+        "2024-06-01",   # str  in 'Date' Textbox component
+        4,  # int | float (numeric value between 1 and 4) in 'n_weeks' Slider component
+        True,   # bool  in 'Use Latest Basic Financials' Checkbox component
+        api_name="/predict"
+)
+
+# Save the result to a JSON file in the current directory
+with open("data/prediction_result.json", "w") as json_file:
+    json.dump(result, json_file)
+ 
+
