f9c803a1c6b5a28c51cb67bdc411c73ea2dbe347
Pai <ppeng24@wisc.edu>
2024-07-30 09:37:56 +0800

    Updated the fetch_news function to correctly format date ranges and modified the ticker variable in the test function.

--- data/sentiment_analysis/api_test.py
+++ data/sentiment_analysis/api_test.py
@@ -10,7 +10,9 @@ password = os.getenv("PASSWORD")
 App_ID = os.getenv("APP_ID")
   
 def fetch_news(month, ticker): 
-    month_str = month.strftime("%Y-%m")  # Format month for URL
+    month_start = month.strftime("%Y-%m-%d")  # Format month for URL
+    month_end = month +=1
+    month_end = month_end.strftime("%Y-%m-%d")  # Format month for URL
     url = f'https://api.aylien.com/v6/news/stories?aql=text: (TESLA) AND sentiment.title.polarity:(negative neutral positive)&cursor=*&published_at.end={month_str}-01T00:00:00.000Z&published_at.start={month_str}-01T00:00:00.000Z'
     # Requesting a bearer token from oauth endpoint
     #Review the docs for detailed authentication workflows docs.aylien.com/newsapi/v6
@@ -20,7 +22,7 @@ def fetch_news(month, ticker):
     headers = {"Authorization": "Bearer {}".format(token), "AppId":"4db0d080"}
     
     #V6 URL
-    url = 'https://api.aylien.com/v6/news/stories?aql=text: (TESLA) AND sentiment.title.polarity:(negative neutral positive)&cursor=*&published_at.end=2022-06-01T00:00:00.000Z&published_at.start=2022-05-01T00:00:00.000Z'
+    url = f'https://api.aylien.com/v6/news/stories?aql=text: ({ticker}) AND sentiment.title.polarity:(negative neutral positive)&cursor=*&published_at.end=2022-06-01T00:00:00.000Z&published_at.start=2022-05-01T00:00:00.000Z'
     
     response = requests.get(url, headers=headers)
     print(response.json())
@@ -28,13 +30,11 @@ def fetch_news(month, ticker):
 
 
 def test_fetch_news():                                       
-     months = [("2022-05"), ("2022-06")]  # List of months to scrape
-     for month in months:
-         news_data = fetch_news(month, ticker)                     
-    ticker = "TESLA"                                         
+    months = [("2022-05"), ("2022-06")]  # List of months to scrape
+    ticker = "TSLKA"
     for month in months:
         news_data = fetch_news(month, ticker)                     
         print(news_data)                                         
                                                               
- if __name__ == "__main__":                                   
-     test_fetch_news(
+if __name__ == "__main__":                                   
+    test_fetch_news()
