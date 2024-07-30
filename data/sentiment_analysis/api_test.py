cc668efe5649d7c1254cb62e34ce2e39847c1066
Pai <ppeng24@wisc.edu>
2024-07-30 09:27:31 +0800

    Added a new API test file for sentiment analysis and created an empty .env file.

---
+++ data/sentiment_analysis/api_test.py
@@ -0,0 +1,20 @@
+import requests
+  
+#Oauth authentication with user credentials
+username = "ppeng24@wisc.edu"
+password = "1234560769wW.."
+#Pre-populated with Production App-Id
+App_ID = "4db0d080"
+  
+#Requesting a bearer token from oauth endpoint
+#Review the docs for detailed authentication workflows docs.aylien.com/newsapi/v6
+token = requests.post("https://api.aylien.com/v1/oauth/token", auth=(username, password), data={"grant_type": "password"}).json()["access_token"]
+  
+#Passing the token as a header with App Id
+headers = {"Authorization": "Bearer {}".format(token), "AppId":"4db0d080"}
+  
+#V6 URL
+url = 'https://api.aylien.com/v6/news/stories?aql=text: (TESLA) AND sentiment.title.polarity:(negative neutral positive)&cursor=*&published_at.end=2022-06-01T00:00:00.000Z&published_at.start=2022-05-01T00:00:00.000Z'
+  
+response = requests.get(url, headers=headers)
+print(response.json())
